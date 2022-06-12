9442a73b9f154550438e1a98ef34505299c563df
Victor <victors.martins20@gmail.com>
2022-06-12 11:09:49 -0300

    Add salvar local de download, otimização no download, correção de bugs

--- YouTube Downloader/Form1.Designer.cs
+++ YouTube Downloader/Form1.Designer.cs
@@ -29,61 +29,66 @@ namespace YouTube_Downloader
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.Btn_baixar = new System.Windows.Forms.Button();
             this.txt_URL = new System.Windows.Forms.TextBox();
-            this.CB_Formato = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.FDB = new System.Windows.Forms.FolderBrowserDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.txt_nomeArq = new System.Windows.Forms.TextBox();
+            this.CB_Formato = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.Btn_baixar = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(35, 36);
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(260, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(306, 33);
+            this.label1.Size = new System.Drawing.Size(156, 33);
             this.label1.TabIndex = 0;
-            this.label1.Text = "YouTube Downloader";
+            this.label1.Text = "Downloader";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(356, 36);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(132, 126);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // Btn_baixar
-            // 
-            this.Btn_baixar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Btn_baixar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Btn_baixar.Location = new System.Drawing.Point(346, 339);
-            this.Btn_baixar.Name = "Btn_baixar";
-            this.Btn_baixar.Size = new System.Drawing.Size(132, 41);
-            this.Btn_baixar.TabIndex = 2;
-            this.Btn_baixar.Text = "Download";
-            this.Btn_baixar.UseVisualStyleBackColor = true;
-            this.Btn_baixar.Click += new System.EventHandler(this.Btn_baixar_Click);
             // 
             // txt_URL
             // 
-            this.txt_URL.Location = new System.Drawing.Point(67, 217);
+            this.txt_URL.Location = new System.Drawing.Point(63, 102);
             this.txt_URL.Name = "txt_URL";
             this.txt_URL.PlaceholderText = "URL do video";
             this.txt_URL.Size = new System.Drawing.Size(421, 23);
             this.txt_URL.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Formato:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(63, 357);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(421, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // txt_nomeArq
+            // 
+            this.txt_nomeArq.Location = new System.Drawing.Point(63, 164);
+            this.txt_nomeArq.Name = "txt_nomeArq";
+            this.txt_nomeArq.PlaceholderText = "Nome do arquivo";
+            this.txt_nomeArq.Size = new System.Drawing.Size(421, 23);
+            this.txt_nomeArq.TabIndex = 9;
             // 
             // CB_Formato
             // 
@@ -95,40 +100,109 @@ namespace YouTube_Downloader
             "MP4 (Video)",
             "MP3 (Áudio)",
             "Ambos"});
-            this.CB_Formato.Location = new System.Drawing.Point(67, 349);
+            this.CB_Formato.Location = new System.Drawing.Point(63, 274);
             this.CB_Formato.Name = "CB_Formato";
             this.CB_Formato.Size = new System.Drawing.Size(121, 23);
-            this.CB_Formato.TabIndex = 4;
+            this.CB_Formato.TabIndex = 10;
             // 
-            // label2
+            // label3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 331);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 15);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Formato:";
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(133, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 33);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "YouTube";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // progressBar1
+            // label4
             // 
-            this.progressBar1.Location = new System.Drawing.Point(67, 462);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(411, 23);
-            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
-            this.progressBar1.TabIndex = 8;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(260, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 33);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Downloader";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(133, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 33);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "YouTube";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(487, 392);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "1.0.2";
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.BackColor = System.Drawing.Color.Transparent;
+            this.btnConfig.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnConfig.BackgroundImage")));
+            this.btnConfig.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnConfig.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConfig.FlatAppearance.BorderSize = 0;
+            this.btnConfig.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfig.Location = new System.Drawing.Point(498, 25);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(33, 27);
+            this.btnConfig.TabIndex = 13;
+            this.toolTip1.SetToolTip(this.btnConfig, "Mudar local de download!");
+            this.btnConfig.UseVisualStyleBackColor = false;
+            this.btnConfig.Click += new System.EventHandler(this.btnConfig_Click);
+            // 
+            // Btn_baixar
+            // 
+            this.Btn_baixar.BackColor = System.Drawing.Color.Red;
+            this.Btn_baixar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn_baixar.FlatAppearance.BorderSize = 0;
+            this.Btn_baixar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Btn_baixar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_baixar.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Btn_baixar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Btn_baixar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Btn_baixar.Location = new System.Drawing.Point(375, 256);
+            this.Btn_baixar.Name = "Btn_baixar";
+            this.Btn_baixar.Size = new System.Drawing.Size(109, 40);
+            this.Btn_baixar.TabIndex = 14;
+            this.Btn_baixar.Text = "Download";
+            this.toolTip1.SetToolTip(this.Btn_baixar, "Download");
+            this.Btn_baixar.UseVisualStyleBackColor = false;
+            this.Btn_baixar.Click += new System.EventHandler(this.Btn_baixar_Click_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(545, 497);
+            this.ClientSize = new System.Drawing.Size(543, 421);
+            this.Controls.Add(this.Btn_baixar);
+            this.Controls.Add(this.btnConfig);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.CB_Formato);
+            this.Controls.Add(this.txt_nomeArq);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.CB_Formato);
             this.Controls.Add(this.txt_URL);
-            this.Controls.Add(this.Btn_baixar);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -136,7 +210,6 @@ namespace YouTube_Downloader
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Home";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,13 +218,19 @@ namespace YouTube_Downloader
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button Btn_baixar;
         private System.Windows.Forms.TextBox txt_URL;
-        private System.Windows.Forms.ComboBox CB_Formato;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.FolderBrowserDialog FDB;
+        private System.Windows.Forms.TextBox txt_nomeArq;
+        private System.Windows.Forms.ComboBox CB_Formato;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button Btn_baixar;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
--- YouTube Downloader/Form1.cs
+++ YouTube Downloader/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using VideoLibrary;
 using MediaToolkit;
 using System.IO;
+using System.Drawing.Drawing2D;
 
 namespace YouTube_Downloader
 {
@@ -19,60 +20,159 @@ namespace YouTube_Downloader
         {
             InitializeComponent();
         }
+        private String LocalArquivo;
+        private bool verificaDownload = false;
 
-
-
-
-
-        private async void Btn_baixar_Click(object sender, EventArgs e)
+        public bool BloqueiaClick(bool verificaDownload)
         {
-
-            if (CB_Formato.Text == "" || txt_URL.Text == "")
+            if(verificaDownload == true)
             {
-                MessageBox.Show("Favor, verificar se você inseriu o link ou se selecionou o formato do arquivo.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Aguarde o download acabar!!");
+                return true;
             }
             else
             {
+                return false;
+            }
+        }
+        public String SalvaLocalDownload()
+        {
+            if (LerArquivoText() == false)
+            {
                 if (FDB.ShowDialog() == DialogResult.OK)
                 {
-                    var yt = YouTube.Default;
-                    var video = await yt.GetVideoAsync(txt_URL.Text);
+                    LocalArquivo = FDB.SelectedPath;
 
-                    File.WriteAllBytes(FDB.SelectedPath + @"\" + video.FullName, await video.GetBytesAsync());
+                    StreamWriter sw = new StreamWriter("LocalDownload.txt");
+                    sw.WriteLine(FDB.SelectedPath);
 
-
-                    var inputfile = new MediaToolkit.Model.MediaFile { Filename = FDB.SelectedPath + @"\" + video.FullName };
-                    var outputfile = new MediaToolkit.Model.MediaFile { Filename = $"{ FDB.SelectedPath + @"\" + video.FullName }.mp3" };
-
-
-                    using (var engine = new Engine())
-                    {
-                        engine.GetMetadata(inputfile);
-                        engine.Convert(inputfile, outputfile);
-                    }
-                    if (CB_Formato.Text == "MP3 (Áudio)")
-                    {
-                        File.Delete(FDB.SelectedPath + @"\" + video.FullName);
-                    }
-                    else if(CB_Formato.Text == "MP4 (Video)")
-                    {
-                        File.Delete($"{FDB.SelectedPath + @"\" + video.FullName}.mp3");
-                    }
-                    else
-                    {
-                        
-                    }
+                    sw.Close();
                 }
                 else
                 {
+                    Btn_baixar.Text = "Download";
+                    MessageBox.Show("Selecione a pasta onde o video/áudio deve ser salvo!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    UseWaitCursor = false;
+                }
+            }
+            return LocalArquivo;
+        }
+        public void GravarLocalArquivo()
+        {
+            if (BloqueiaClick(verificaDownload) == false)
+            {
+                if (FDB.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter sw = new StreamWriter("LocalDownload.txt");
+                    sw.WriteLine(FDB.SelectedPath);
+                    sw.Close();
+                }
+                else
+                {
+                    Btn_baixar.Text = "Download";
                     MessageBox.Show("Selecione a pasta onde o video/áudio deve ser salvo!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-
-                progressBar1.Value = 100;
-                MessageBox.Show("Download Concluido!", "Concluido", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
+        }
+        public bool LerArquivoText()
+        {
+            if (File.Exists("LocalDownload.txt"))
+            {
+                foreach (string line in System.IO.File.ReadLines(@"LocalDownload.txt"))
+                {
+                    LocalArquivo = line;
+                    Console.ReadLine();
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void VerificaCampos(String LocalArquivo)
+        {
+            if (CB_Formato.Text == "" || txt_URL.Text == "")
+            {
+                MessageBox.Show("Favor, verificar se você inseriu o link ou se selecionou o formato do arquivo.", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                UseWaitCursor = false;
+            }
+            else
+            {
+                SalvaLocalDownload();
+            }
+        }
+        public async Task RealizaDownload(String LocalArquivo)
+        {
+            if (BloqueiaClick(verificaDownload) == false)
+            {
+                progressBar1.Value = 0;
+                UseWaitCursor = true;
+                verificaDownload = true;
+
+                var yt = YouTube.Default;
+                var video = await yt.GetVideoAsync(txt_URL.Text);
+                var nomeDArq = txt_nomeArq.Text;
+
+                Btn_baixar.Text = "Baixando...";
+
+                File.WriteAllBytes(LocalArquivo + @"\" + nomeDArq + ".mp4", await video.GetBytesAsync());
+
+                var outputfile = new MediaToolkit.Model.MediaFile { Filename = $"{ LocalArquivo + @"\" + nomeDArq + "" }.mp3" };
+                var inputfile = new MediaToolkit.Model.MediaFile { Filename = LocalArquivo + @"\" + nomeDArq + ".mp4" };
 
 
+                var engine = new Engine();
+                engine.GetMetadata(inputfile);
+                engine.Convert(inputfile, outputfile);
+
+                if (CB_Formato.Text == "MP3 (Áudio)")
+                {
+                    File.Delete(LocalArquivo + @"\" + nomeDArq + ".mp4");
+                }
+                else if (CB_Formato.Text == "MP4 (Video)")
+                {
+                    File.Delete($"{LocalArquivo + @"\" + nomeDArq + ""}.mp3");
+                }
+                ConcluidoEvent();
+            }
+        }
+        public void ConcluidoEvent()
+        {
+            progressBar1.Value = 100;
+            Btn_baixar.Text = "Download";
+            MessageBox.Show("Download Concluido!", "Concluido", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            UseWaitCursor = false;
+            verificaDownload = false;
+            progressBar1.Value = 0;
+        }
+        private void Btn_baixar_Click_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VerificaCampos(LocalArquivo);
+                RealizaDownload(LocalArquivo);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro:" +
+                    "n\n" +
+                    ex.Message);
+            }
+        }
+        private void btnConfig_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GravarLocalArquivo();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro:" +
+                    "n\n" +
+                    ex.Message);
+            }
         }
     }
 }
